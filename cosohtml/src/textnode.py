65b0b3817a6f5e4e378c4e6b92fc1ec88e7f6629
Facundo Adrián Lucianna <facundolucianna@gmail.com>
2026-01-13 23:09:15 -0300

    feat: Implement `text_node_to_html_node` function, refactor `TextType` enum members, and add comprehensive tests for `TextNode` conversion and `HtmlNode` classes.

--- cosohtml/src/textnode.py
+++ cosohtml/src/textnode.py
@@ -1,11 +1,11 @@
 from enum import Enum
-
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    NORMAL_TEXT = "text"
-    BOLD_TEXT = "bold_text"
-    ITALIC_TEXT = "italic_text"
-    CODE_TEXT = "code_text"
+    TEXT = "text"
+    BOLD = "bold_text"
+    ITALIC = "italic_text"
+    CODE = "code_text"
     LINK = "link"
     IMAGE = "image"
 
@@ -28,3 +28,21 @@ class TextNode:
         if self.url:
             repres += f", {self.url}"
         return repres + ")"
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, props={"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", props={"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError(f"Invalid text type: {text_node.text_type}")
