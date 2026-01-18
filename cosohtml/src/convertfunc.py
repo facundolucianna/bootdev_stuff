d2a3f4ddbe596848c1f99cfffc2054b362f4def6
Facundo Adrián Lucianna <facundolucianna@gmail.com>
2026-01-17 22:48:53 -0300

    feat: Implement functions to split text nodes by markdown images and links, including tests and examples.

--- cosohtml/src/convertfunc.py
+++ cosohtml/src/convertfunc.py
@@ -53,3 +53,65 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
             new_nodes.append(node)
 
     return new_nodes
+
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+
+        images = extract_markdown_images(node.text)
+
+        if len(images) == 0:
+            new_nodes.append(node)
+            continue
+        
+        image_nodes = []
+        initial_flag = True
+        all_text = node.text.strip()
+        for image in images:
+            text_split = all_text.split(f"![{image[0]}]({image[1]})", 1)
+            if text_split[0] != "":
+                if not initial_flag:
+                    image_nodes.append(TextNode(" " + text_split[0].strip() + " ", TextType.TEXT))
+                else:
+                    image_nodes.append(TextNode(text_split[0].strip() + " ", TextType.TEXT))
+            image_nodes.append(TextNode(image[0], TextType.IMAGE, url=image[1]))
+            all_text = " " + text_split[1]
+            initial_flag = False
+            
+        if all_text.strip() != "" :
+            image_nodes.append(TextNode(" " + all_text.strip(), TextType.TEXT))
+
+        new_nodes.extend(image_nodes)
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            links = extract_markdown_links(node.text)
+            if len(links) == 0:
+                new_nodes.append(node)
+                continue
+            link_nodes = []
+            initial_flag = True
+            all_text = node.text.strip()
+            for link in links:
+                text_split = all_text.split(f"[{link[0]}]({link[1]})", 1)
+                if text_split[0] != "":
+                    if not initial_flag:
+                        link_nodes.append(TextNode(" " + text_split[0].strip() + " ", TextType.TEXT))
+                    else:
+                        link_nodes.append(TextNode(text_split[0].strip() + " ", TextType.TEXT))
+                link_nodes.append(TextNode(link[0], TextType.LINK, url=link[1]))
+                all_text = " " + text_split[1]
+                initial_flag = False
+            
+            if all_text.strip() != "":
+                link_nodes.append(TextNode(" " + all_text.strip(), TextType.TEXT))
+            new_nodes.extend(link_nodes)
+        else:
+            new_nodes.append(node)
+    return new_nodes
