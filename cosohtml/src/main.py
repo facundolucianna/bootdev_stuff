8d4b6c344fb4836c313a10367dfc2a5e3c67894d
Facundo Adrián Lucianna <facundolucianna@gmail.com>
2026-01-19 08:02:40 -0300

    feat: implement `markdown_to_html_node` to convert markdown blocks into HTML node structures with corresponding tests and usage.

--- cosohtml/src/main.py
+++ cosohtml/src/main.py
@@ -1,4 +1,6 @@
 from textnode import TextNode, TextType
+from markdownblocks import markdown_to_blocks, block_to_block_type, BlockType, markdown_to_html_node
+
 
 
 def main():
@@ -6,6 +8,22 @@ def main():
         "This is some anchor text", TextType.LINK, "https://www.boot.dev"
     )
     print(new_obj)
+    
+    md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+    blocks = markdown_to_html_node(md)
+    #print(blocks)
+    for block in blocks.children:
+        print(block)
+
+    
+    
 
 
 main()
--- cosohtml/src/markdownblocks.py
+++ cosohtml/src/markdownblocks.py
@@ -2,6 +2,8 @@ import re
 
 from enum import Enum
 
+from htmlnode import LeafNode, ParentNode
+
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
     HEADING = "heading"
@@ -50,3 +52,48 @@ def block_to_block_type(block: str) -> BlockType:
             return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
 
+
+def markdown_to_html_node(markdown):
+    pre_formed_blocks = markdown_to_blocks(markdown)
+    nodes = []
+    for block in pre_formed_blocks:
+        block_type = block_to_block_type(block)
+        new_block = block.strip()
+        match block_type:    
+            case BlockType.HEADING:
+
+                level = len(re.match(r"^#{1,6} ", new_block).group(0)) - 1 
+                nodes.append(LeafNode(tag=f"h{level}", value=new_block[level+1:].strip()))
+
+            case BlockType.CODE:
+                new_block = new_block[4:-3]
+                new_node = LeafNode(tag="code", value=new_block)
+                nodes.append(ParentNode(tag="pre", children=[new_node]))
+
+            case BlockType.QUOTE:
+                new_block = new_block[2:]
+                new_node = LeafNode(tag="blockquote", value=new_block)
+                nodes.append(new_node)
+                
+            case BlockType.UNORDERED_LIST:
+                child_list = []
+                for linea in new_block.splitlines():
+                    child_list.append(LeafNode(tag="li", value=linea[2:].strip()))
+                nodes.append(ParentNode(tag="ul", children=child_list))
+                
+            case BlockType.ORDERED_LIST:
+                child_list = []
+                for linea in new_block.splitlines():
+                    level = len(re.match(r"^\d+\. ", linea).group(0))
+                    child_list.append(LeafNode(tag="li", value=linea[level:].strip()))
+                nodes.append(ParentNode(tag="ol", children=child_list))
+                
+            case BlockType.PARAGRAPH:
+                nodes.append(LeafNode(tag="p", value=new_block))
+                
+            case _:
+                raise ValueError(f"Unknown block type: {block_type}")
+        
+    return ParentNode(tag="div", children=nodes)
+            
+        
