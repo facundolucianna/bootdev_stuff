9d9e271823b28658b1aa319738d7505bc457681f
Facundo Adrián Lucianna <facundolucianna@gmail.com>
2026-01-25 14:48:38 -0300

    feat: Rename build output directory from `public` to `docs` and add basepath support for generated site links.

--- cosohtml/src/generate_funcs.py
+++ cosohtml/src/generate_funcs.py
@@ -16,7 +16,7 @@ def extract_title(markdown: str) -> str:
     raise ValueError("No heading found in markdown")
 
 
-def generate_page(from_path: Path, template_path: Path, dest_path: Path):
+def generate_page(from_path: Path, template_path: Path, dest_path: Path, basepath: str):
     print(f"Generating page from {from_path.resolve()} to {dest_path.resolve()} using {template_path.resolve()}")
 
     if not dest_path.parent.exists():
@@ -30,16 +30,19 @@ def generate_page(from_path: Path, template_path: Path, dest_path: Path):
 
     new_page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
 
+    new_page = new_page.replace('href="/', f'href="{basepath}')
+    new_page = new_page.replace('src="/', f'src="{basepath}')
+
     dest_path.write_text(new_page)
 
     print("Done")
 
 
-def generate_pages_recursively(dir_path_content: Path, template_path: Path, dest_dir_path: Path):
+def generate_pages_recursively(dir_path_content: Path, template_path: Path, dest_dir_path: Path, basepath: str):
 
     for file in dir_path_content.iterdir():
         if file.is_file():
             if file.name.endswith(".md"):
-                generate_page(file, template_path, dest_dir_path / file.name.replace(".md", ".html"))
+                generate_page(file, template_path, dest_dir_path / file.name.replace(".md", ".html"), basepath)
         elif file.is_dir():
-            generate_pages_recursively(file, template_path, dest_dir_path / file.name) 
+            generate_pages_recursively(file, template_path, dest_dir_path / file.name, basepath) 
--- cosohtml/src/main.py
+++ cosohtml/src/main.py
@@ -1,14 +1,21 @@
 from osfunc import clean_and_copy
 from pathlib import Path
 from generate_funcs import generate_pages_recursively
+import sys 
 
 def main():
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    print(f"Using basepath: {basepath}")
+
     print("Copying static files to public directory")
-    clean_and_copy(Path("./static"), Path("./public"))
+    clean_and_copy(Path("static"), Path("docs"))
     print("Done")
 
     print("Generating pages recursively")
-    generate_pages_recursively(Path("./content"), Path("./template.html"), Path("./public"))
+    generate_pages_recursively(Path("content"), Path("template.html"), Path("docs"), basepath)
     print("Done")
 
 main()
